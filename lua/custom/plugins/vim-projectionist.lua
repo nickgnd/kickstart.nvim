8ce27a824b4bcd1d0e4970ad287f9296f3fa8398
Nicolò Gnudi <nicolo.gnudi@gmail.com>
2024-07-21 14:56:42 +0200

    chore: install `vim-projectionist` plugin

---
+++ lua/custom/plugins/vim-projectionist.lua
@@ -0,0 +1,33 @@
+-- Project configuration
+return {
+  'tpope/vim-projectionist',
+  config = function()
+    vim.g.projectionist_heuristics = {
+      ['mix.exs'] = {
+        ['*'] = {
+          console = 'iex -S mix',
+          make = 'mix',
+        },
+        ['lib/*.ex'] = {
+          type = 'source',
+          alternate = 'test/{}_test.exs',
+          template = {
+            'defmodule {camelcase|capitalize|dot} do',
+            'end',
+          },
+        },
+        ['test/*_test.exs'] = {
+          type = 'test',
+          alternate = 'lib/{}.ex',
+          template = {
+            'defmodule {camelcase|capitalize|dot}Test do',
+            '  use ExUnit.Case, async: true',
+            '',
+            '  alias {camelcase|capitalize|dot}',
+            'end',
+          },
+        },
+      },
+    }
+  end,
+}
