044a6d0c02b60d544f0c45e4b60f4d39cc729052
Nicolò Gnudi <nicolo.gnudi@gmail.com>
2024-11-11 10:18:24 +0100

    chore: nvim - install copilot

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,20 @@
+-- Copilot plugin and transform it into a cmp source
+-- https://github.com/zbirenbaum/copilot-cmp
+--
+return {
+  'zbirenbaum/copilot-cmp',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot_cmp').setup()
+  end,
+  dependencies = {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    config = function()
+      require('copilot').setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+}
