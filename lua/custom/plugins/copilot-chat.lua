4417d91ab70811a01ad7eea5ed6b24f65dbfcc34
Nicolò Gnudi <nicolo.gnudi@gmail.com>
2025-02-16 23:16:34 +0100

    chore: enable quick chat with copilot

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -14,6 +14,18 @@ return {
       -- See Configuration section for options
       model = 'claude-3.5-sonnet',
     },
+    keys = {
+      {
+        '<leader>ccq',
+        function()
+          local input = vim.fn.input 'Quick Chat: '
+          if input ~= '' then
+            require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+          end
+        end,
+        desc = 'CopilotChat - Quick chat',
+      },
+    },
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
