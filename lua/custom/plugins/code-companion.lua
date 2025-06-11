5b58ad52d9b2bcb7f53329f0a898cc64baca9983
Nicolò Gnudi <nicolo.gnudi@gmail.com>
2025-06-11 14:38:54 +0200

    chore: install codecompanion-history plugin

--- lua/custom/plugins/code-companion.lua
+++ lua/custom/plugins/code-companion.lua
@@ -9,6 +9,7 @@ return {
     { 'nvim-lua/plenary.nvim', branch = 'master' },
     'nvim-treesitter/nvim-treesitter',
     'ravitemer/mcphub.nvim',
+    'ravitemer/codecompanion-history.nvim',
     {
       'saghen/blink.cmp',
       ---@module 'blink.cmp'
@@ -134,6 +135,37 @@ return {
           make_vars = true,
           make_slash_commands = true,
           show_result_in_chat = true,
+        },
+      },
+      history = {
+        enabled = true,
+        opts = {
+          -- Keymap to open history from chat buffer (default: gh)
+          keymap = 'gh',
+          -- Keymap to save the current chat manually (when auto_save is disabled)
+          save_chat_keymap = 'sc',
+          -- Save all chats by default (disable to save only manually using 'sc')
+          auto_save = false,
+          -- Number of days after which chats are automatically deleted (0 to disable)
+          expiration_days = 0,
+          -- Picker interface ("telescope" or "snacks" or "fzf-lua" or "default")
+          picker = 'snacks',
+          ---Automatically generate titles for new chats
+          auto_generate_title = true,
+          title_generation_opts = {
+            ---Adapter for generating titles (defaults to active chat's adapter)
+            adapter = nil, -- e.g "copilot"
+            ---Model for generating titles (defaults to active chat's model)
+            model = nil, -- e.g "gpt-4o"
+          },
+          ---On exiting and entering neovim, loads the last chat on opening chat
+          continue_last_chat = false,
+          ---When chat is cleared with `gx` delete the chat from history
+          delete_on_clearing_chat = false,
+          ---Directory path to save the chats
+          dir_to_save = vim.fn.stdpath 'data' .. '/codecompanion-history',
+          ---Enable detailed logging for history extension
+          enable_logging = false,
         },
       },
     },
