4b9040eb8f22578712c6a44aee6f0056782cdc3f
Nicolò Gnudi <nicolo.gnudi@gmail.com>
2025-07-04 12:28:22 +0200

    chore: add keymap to toggle diagnostic messages

--- init.lua
+++ init.lua
@@ -512,6 +512,11 @@ require('lazy').setup({
           --  Similar to document symbols, except searches over your entire project.
           map('gW', require('snacks').picker.lsp_workspace_symbols, 'Open Workspace Symbols')
 
+          -- Toggle to show/hide diagnostic messages
+          map('<leader>td', function()
+            vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+          end, '[T]oggle [D]iagnostics')
+
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
