280bea08fe16241c157eee4ab0bb7a9f6f97a4b6
Nicolò Gnudi <nicolo.gnudi@gmail.com>
2025-07-04 12:29:52 +0200

    chore: add keymap for gitsigns preview hunk inline

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -50,6 +50,7 @@ return {
         map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
         map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
         map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+        map('n', '<leader>hl', gitsigns.preview_hunk_inline, { desc = 'git [d]iff hunk inline' })
         map('n', '<leader>hD', function()
           gitsigns.diffthis '@'
         end, { desc = 'git [D]iff against last commit' })
