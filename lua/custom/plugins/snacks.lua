909b180864add7804be4aca98da07c1eace959b1
Nicolò Gnudi <nicolo.gnudi@gmail.com>
2025-11-03 21:46:16 +0100

    chore(snacks): trying out `gh` snacks utility

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -61,6 +61,11 @@ return {
         { section = 'startup' },
       },
     },
+    gh = {
+      -- your gh configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
     lazygit = { enabled = false },
     notifier = { enabled = true },
     picker = {
@@ -82,6 +87,14 @@ return {
       },
       sources = {
         explorer = {},
+        gh_issue = {
+          -- your gh_issue picker configuration comes here
+          -- or leave it empty to use the default settings
+        },
+        gh_pr = {
+          -- your gh_pr picker configuration comes here
+          -- or leave it empty to use the default settings
+        },
       },
       actions = {
         -- Send picker selections directly to Sidekick's AI CLI tools with `Alt+a`
@@ -263,7 +276,35 @@ return {
       end,
       desc = '[S]earch [N]eovim files',
     },
-    --
+    -- Gh
+    {
+      '<leader>gi',
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = 'GitHub Issues (open)',
+    },
+    {
+      '<leader>gI',
+      function()
+        Snacks.picker.gh_issue { state = 'all' }
+      end,
+      desc = 'GitHub Issues (all)',
+    },
+    {
+      '<leader>gp',
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = 'GitHub Pull Requests (open)',
+    },
+    {
+      '<leader>gP',
+      function()
+        Snacks.picker.gh_pr { state = 'all' }
+      end,
+      desc = 'GitHub Pull Requests (all)',
+    },
     -- Scratch
     {
       '<leader>.',
